4c29dcda29cd3b01be50e87a58e7b3cdbda23488
adif-seopage1 <maynul@gmail.com>
2026-07-22 10:30:46 +0600

    refactored page

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,11 @@
-import Dashboard from "@/components/Dashboard";
-import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
-import InsightFlow from "@/components/InsightFlow";
 import Navbar from "@/components/Navbar";
-import SignatureInteraction from "@/components/SignatureInteraction";
 
 export default function Home() {
   return (
     <main className="relative">
       <Navbar />
       <Hero />
-      <InsightFlow />
-      <Dashboard />
-      <SignatureInteraction />
-      <Footer />
     </main>
   );
 }
